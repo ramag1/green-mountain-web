2fb06fda49b4e554cfc7ee89d788cbb62ea7e287
rmagley <rachelmagley@gmail.com>
2022-06-27 11:17:41 -0400

    Add mobile stlying to about us page

--- green-mountain/src/components/AboutUs.js
+++ green-mountain/src/components/AboutUs.js
@@ -4,49 +4,48 @@ import './AboutUs.css'
 
 function AboutUs(props) {
     return (
-			<div id='about'>
-				<div className='about__div'>
-					<p className='about__p'>
-						Co-founder Joseph S. Dicton, Jr. is the only child of Joseph Sr. and
-						Marian Dicton. The Dicton 22-acre homestead was purchased in 1978
-						and the three worked to clear the land and establish Joe’s Nursery
-						and Landscaping, a going concern until Joe Sr. passed in 2003. Long
-						roots in the nursery business from cultivation through retail sales
-						and installation of same, Joseph Jr. earned his green thumb. Joe is
-						deeply rooted and very connected to his community, serving on the
-						Rutland Town Fire Department and Rutland Town Select Board, also as
-						its chairman. Always loving the horticultural business, he continues
-						to get his hands dirty and garden while working full time with Green
-						Mountain Power. Stephanie Brush, co-founder, spent a business career
-						growing clients. Two decades of working for multi-national companies
-						in the technology sector focused on Business Development brings a
-						unique skill set for nurturing relationships, collaboration, and
-						problem solving. Growing up in Middletown, PA her Family planted a
-						vegetable garden every Summer – tomatoes, lettuce, beets, cucumbers,
-						and anything else that fills a salad bowl. All that was to a lesser
-						degree than her father, Ted, grew up tending with his 12 siblings.
-						Always people around to share a laugh and a meal. Stephanie’s love
-						of cooking with fresh ingredients and entertaining began early.
-						Nurturing loved ones is at the core of who she is as a person. Our
-						cannabis journey began in that nurturing way during Summer 2018 with
-						the advent of legal recreational cannabis use in Vermont. After a
-						lifetime of suffering intense migraine headaches, Joe was curious
-						about the many purported healing properties and so we grew 2 weed
-						plants fondly nicknamed Chip & Dip. Joe is migraine free today and
-						his great outcome is attributed to healing cannabis. We have a joint
-						passion and talents to grow natural, handcrafted, healing cannabis
-						for its many benefits and the enjoyment of Vermonters. Experiencing
-						the healthful properties of cannabis and envisioning opportunity to
-						reclaim those roots and branch into cannabis cultivation is a
-						natural melding of both Joe’s and Stephanie’s talents and a welcomed
-						business proposition. Green Mountain Ganja Guys was established to
-						fulfill that vision. We are uniquely positioned to that end as
-						landowners with cultivation experience in the heart of our Green
-						Mountain state for decades.
-					</p>
-				</div>
-			</div>
-		);
+		<div className='about__div'>
+			<p className='about__p'>
+				Co-founder Joseph S. Dicton, Jr. is the only child of Joseph Sr. and
+				Marian Dicton. The Dicton 22-acre homestead was purchased in 1978
+				and the three worked to clear the land and establish Joe’s Nursery
+				and Landscaping, a going concern until Joe Sr. passed in 2003. Long
+				roots in the nursery business from cultivation through retail sales
+				and installation of same, Joseph Jr. earned his green thumb. <br/>
+				Joe is deeply rooted and very connected to his community, serving on the
+				Rutland Town Fire Department and Rutland Town Select Board, also as
+				its chairman. Always loving the horticultural business, he continues
+				to get his hands dirty and garden while working full time with Green
+				Mountain Power. <br/>
+				Stephanie Brush, co-founder, spent a business career
+				growing clients. Two decades of working for multi-national companies
+				in the technology sector focused on Business Development brings a
+				unique skill set for nurturing relationships, collaboration, and
+				problem solving. Growing up in Middletown, PA her Family planted a
+				vegetable garden every Summer – tomatoes, lettuce, beets, cucumbers,
+				and anything else that fills a salad bowl. All that was to a lesser
+				degree than her father, Ted, grew up tending with his 12 siblings.
+				Always people around to share a laugh and a meal. Stephanie’s love
+				of cooking with fresh ingredients and entertaining began early.
+				Nurturing loved ones is at the core of who she is as a person. Our
+				cannabis journey began in that nurturing way during Summer 2018 with
+				the advent of legal recreational cannabis use in Vermont. After a
+				lifetime of suffering intense migraine headaches, Joe was curious
+				about the many purported healing properties and so we grew 2 weed
+				plants fondly nicknamed Chip & Dip. Joe is migraine free today and
+				his great outcome is attributed to healing cannabis. We have a joint
+				passion and talents to grow natural, handcrafted, healing cannabis
+				for its many benefits and the enjoyment of Vermonters. Experiencing
+				the healthful properties of cannabis and envisioning opportunity to
+				reclaim those roots and branch into cannabis cultivation is a
+				natural melding of both Joe’s and Stephanie’s talents and a welcomed
+				business proposition. Green Mountain Ganja Guys was established to
+				fulfill that vision. We are uniquely positioned to that end as
+				landowners with cultivation experience in the heart of our Green
+				Mountain state for decades.
+			</p>
+		</div>
+	);
 }
 
 export default AboutUs;
